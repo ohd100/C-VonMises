2ced44a1611964a598f3709f319da655f059cb01
ohd100 <ohd100@utulsa.edu>
2017-10-28 18:30:32 -0500

    10/28

---
+++ ConsoleApplication1/Class1.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Runtime.InteropServices;
+
+namespace ConsoleApplication1
+{
+    public class radtanStress
+    {
+        public static double radStress(double OD, double ID, double Pi, double Po, bool inwall)
+        {
+            double Ro; double Ri; double r;
+            double a; double b;
+
+            /*'Using Lame correlations
+            'OD, ID in inches
+            'Pi and Po in psi
+            ' inwalloutwall of 1 measures stress at inner wall; value of 0 measures stress at outer wall
+            */
+
+            Ro = OD / 2;
+            Ri = ID / 2;
+
+            if (inwall == true)
+                r = Ri;
+            else
+                r = Ro;
+
+
+
+
+            a = ((-(Ro * Ro) * (Ri * Ri) * (Pi - Po))) / ((((Ro * Ro) - (Ri * Ri)) * (r * r)));
+            b = ((Math.Pow(Ri, 2) * Pi) - (Math.Pow(Ro, 2) * Po)) / (Math.Pow(Ro, 2) - Math.Pow(Ri, 2));
+
+            double radStress = a + b;
+
+            return radStress;
+            //'Print #1, "sigR" & vbTab & radStress
+        }
+
+        public static double tanStress(double OD, double ID, double Pi, double Po, bool inwall)
+        {
+            double Ro; double Ri; double r;
+            double a; double b;
+
+            /*'Using Lame correlations
+            'OD, ID in inches
+            'Pi and Po in psi
+            ' inwalloutwall of 1 measures stress at inner wall; value of 0 measures stress at outer wall
+            */
+
+            Ro = OD / 2;
+            Ri = ID / 2;
+
+            if (inwall == true)
+                r = Ri;
+            else
+                r = Ro;
+
+
+
+
+            a = (((Ro * Ro) * (Ri * Ri) * (Pi - Po))) / ((((Ro * Ro) - (Ri * Ri)) * (r * r)));
+            b = ((Math.Pow(Ri, 2) * Pi) - (Math.Pow(Ro, 2) * Po)) / (Math.Pow(Ro, 2) - Math.Pow(Ri, 2));
+
+            double tanStress = a + b;
+
+
+            return tanStress;
+            //'Print #1, "sigR" & vbTab & radStress
+        }
+
+    }
+    public class interpolate
+    {
+        public double[,] pullCasingSpecsExcel()
+        {
+            //Excel = Microsoft.Office.Interop.Excel;
+            Excel.Application xlApp = new Excel.Application();
+            Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(@"D:\Completions\CasingSpecs.xlsm");
+            Excel._Worksheet xlWorksheet = xlWorkbook.Sheets["CasingInputs"];
+            Excel.Range xlRange = xlWorksheet.UsedRange;
+
+            //int usedCols = xlRange.Columns;
+
+            //xLrange = (Excel.Range)(xlWorkSheet.UsedRange.Columns
+
+            int numRows = xlWorksheet.UsedRange.Rows.Count;
+            int numCols = xlWorksheet.UsedRange.Columns.Count;
+
+            double[,] xLarray = new double[numRows - 1, numCols];
+
+            for (int i = 2; i <= numRows; i++)
+            {
+                for (int j = 1; j <= numCols; j++)
+                {
+                    xLarray[i - 2, j - 1] = Convert.ToDouble(xlWorksheet.Cells[i, j].Value);
+                }
+            }
+
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+            Marshal.ReleaseComObject(xlRange);
+            Marshal.ReleaseComObject(xlWorksheet);
+
+            //close and release
+            xlWorkbook.Close();
+            Marshal.ReleaseComObject(xlWorkbook);
+
+            //quit and release
+            xlApp.Quit();
+            Marshal.ReleaseComObject(xlApp);
+
+
+
+            return xLarray;
+        }
+
+        public double[,] pullDesignLinesExcel()
+        {
+            //Excel = Microsoft.Office.Interop.Excel;
+            Excel.Application xlApp = new Excel.Application();
+            Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(@"D:\Completions\CasingSpecs.xlsm");
+            Excel._Worksheet xlWorksheet = xlWorkbook.Sheets["BurstCollapseDesign"];
+            Excel.Range xlRange = xlWorksheet.UsedRange;
+
+            //int usedCols = xlRange.Columns;
+
+            //xLrange = (Excel.Range)(xlWorkSheet.UsedRange.Columns
+
+            int numRows = xlWorksheet.UsedRange.Rows.Count;
+            int numCols = xlWorksheet.UsedRange.Columns.Count;
+
+            double[,] xLarray = new double[numRows - 1, numCols];
+
+            for (int i = 2; i <= numRows; i++)
+            {
+                for (int j = 1; j <= numCols; j++)
+                {
+                    xLarray[i - 2, j - 1] = Convert.ToDouble(xlWorksheet.Cells[i, j].Value);
+                }
+            }
+
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+            Marshal.ReleaseComObject(xlRange);
+            Marshal.ReleaseComObject(xlWorksheet);
+
+            //close and release
+            xlWorkbook.Close();
+            Marshal.ReleaseComObject(xlWorkbook);
+
+            //quit and release
+            xlApp.Quit();
+            Marshal.ReleaseComObject(xlApp);
+
+
+
+            return xLarray;
+        }
+
+        public static int interpolateBinary(double[] inp, double x, int column)
+        {
+            int searchIndex; double upperVal; double lowerVal;
+            int index1 = Array.BinarySearch(inp, x);
+            double interpolatedValue;
+
+            searchIndex = index1;
+
+
+            return searchIndex;
+        }
+
+        public List<double> arrayToList(double[,] inp, int column)
+        {
+            var retList = new List<double>();
+
+            int numRow = inp.GetLength(0);
+            //int numCol = inp.GetLength(1);
+            for (int i = 0; i <= numRow - 1; i++)
+            {
+                retList.Add(inp[i, column - 1]);
+            }
+
+            return retList;
+        }
+    }
+
+    public class trajectory
+    {
+        public double[,] buildTrajectory(double KOP, double DLS, double buildsection, double finalinclination, double MD, double intervalstep)
+        {
+            double critpoint; double endbuild;
+            int numsteps = Convert.ToInt32((MD / intervalstep));
+            double[,] trajectory = new double[numsteps+1, 2];
+            //    'col 1: MD
+            //    'col 2: Inclination angles
+            endbuild = MD - (MD - buildsection - KOP);
+
+            //'Populating MDs in trajectory array
+                for (int i = 1; i <= numsteps+1; i++)
+                {
+                    trajectory[i - 1, 0] = (i - 1) * intervalstep;
+                }
+
+            //'Populating inclination angles
+                for (int j = 1; j <= numsteps+1; j++)
+                {
+                    if (trajectory[j-1, 0] < KOP)
+                    {
+                        trajectory[j-1, 1] = 0;
+                    }
+                    else if (trajectory[j-1, 0] > endbuild)
+                    {
+                        trajectory[j-1, 1] = finalinclination;
+                    }
+                    else
+                    {
+                        trajectory[j-1, 1] = trajectory[j - 2, 1] + (DLS / 100) * intervalstep;
+                    }   
+                }
+                    
+
+
+                //i = 1
+                //j = 1
+
+                //'Populating MDs in trajectory array
+                //For i = 1 To numsteps + 1
+                //    trajectory(i, 1) = (i - 1) * intervalstep
+                //Next i
+
+
+                //'Populating inclination angles
+                //For j = 1 To numsteps + 1
+                //    If trajectory(j, 1) < KOP Then
+                //        trajectory(j, 2) = 0
+                //    ElseIf trajectory(j, 1) > endbuild Then
+                //        trajectory(j, 2) = finalinclination
+                //    Else: trajectory(j, 2) = trajectory(j - 1, 2) + (DLS / 100) * intervalstep
+                //    End If
+                //Next j
+
+                return trajectory;
+        }
+    }
+}
--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -8,196 +8,7 @@ using System.Runtime.InteropServices;
 
 namespace ConsoleApplication1
 {
-    public class radtanStress
-    {
-        public static double radStress(double OD,double ID,double Pi,double Po,bool inwall)
-        {
-                double Ro; double Ri; double r;
-                double a; double b;
-    
-                /*'Using Lame correlations
-                'OD, ID in inches
-                'Pi and Po in psi
-                ' inwalloutwall of 1 measures stress at inner wall; value of 0 measures stress at outer wall
-                */
-                
-                Ro = OD / 2;
-                Ri = ID / 2;
-    
-                if (inwall == true)
-                    r = Ri;
-                else 
-                    r = Ro;
-                
-                
-    
-
-                a = ((-(Ro * Ro) * (Ri * Ri) * (Pi - Po))) / ((((Ro * Ro) - (Ri * Ri)) * (r * r)));
-                b = ((Math.Pow(Ri,2) * Pi) - (Math.Pow(Ro,2) * Po)) / (Math.Pow(Ro,2) - Math.Pow(Ri,2));
-
-                double radStress = a + b;
-
-                return radStress;
-                //'Print #1, "sigR" & vbTab & radStress
-        }
-
-        public static double tanStress(double OD, double ID, double Pi, double Po, bool inwall)
-        {
-            double Ro; double Ri; double r;
-            double a; double b;
-
-            /*'Using Lame correlations
-            'OD, ID in inches
-            'Pi and Po in psi
-            ' inwalloutwall of 1 measures stress at inner wall; value of 0 measures stress at outer wall
-            */
-
-            Ro = OD / 2;
-            Ri = ID / 2;
-    
-            if (inwall==true)
-                r = Ri;
-            else 
-                r = Ro;
-            
-    
-    
-
-            a = (((Ro * Ro) * (Ri * Ri) * (Pi - Po))) / ((((Ro * Ro) - (Ri * Ri)) * (r * r)));
-            b = ((Math.Pow(Ri, 2) * Pi) - (Math.Pow(Ro, 2) * Po)) / (Math.Pow(Ro, 2) - Math.Pow(Ri, 2));
-
-            double tanStress = a + b;
-
-
-            return tanStress;
-            //'Print #1, "sigR" & vbTab & radStress
-        }
-
-    }
-    public class interpolate
-    {
-        public double[,] pullCasingSpecsExcel()
-        {
-            //Excel = Microsoft.Office.Interop.Excel;
-            Excel.Application xlApp = new Excel.Application();
-            Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(@"D:\Completions\CasingSpecs.xlsm");
-            Excel._Worksheet xlWorksheet = xlWorkbook.Sheets["CasingInputs"];
-            Excel.Range xlRange = xlWorksheet.UsedRange;
-
-            //int usedCols = xlRange.Columns;
-
-            //xLrange = (Excel.Range)(xlWorkSheet.UsedRange.Columns
-
-            int numRows = xlWorksheet.UsedRange.Rows.Count;
-            int numCols = xlWorksheet.UsedRange.Columns.Count;
-
-            double[,] xLarray = new double[numRows-1, numCols];
-
-            for (int i = 2; i <= numRows; i++)
-            {
-                for (int j = 1; j <= numCols; j++)
-                {
-                    xLarray[i - 2, j - 1] = Convert.ToDouble(xlWorksheet.Cells[i, j].Value);
-                }
-            }
-
-            GC.Collect();
-            GC.WaitForPendingFinalizers();
-
-            Marshal.ReleaseComObject(xlRange);
-            Marshal.ReleaseComObject(xlWorksheet);
-
-            //close and release
-            xlWorkbook.Close();
-            Marshal.ReleaseComObject(xlWorkbook);
-
-            //quit and release
-            xlApp.Quit();
-            Marshal.ReleaseComObject(xlApp);
-
-
-
-            return xLarray;
-        }
-
-        public double[,] pullDesignLinesExcel()
-        {
-            //Excel = Microsoft.Office.Interop.Excel;
-            Excel.Application xlApp = new Excel.Application();
-            Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(@"D:\Completions\CasingSpecs.xlsm");
-            Excel._Worksheet xlWorksheet = xlWorkbook.Sheets["BurstCollapseDesign"];
-            Excel.Range xlRange = xlWorksheet.UsedRange;
-
-            //int usedCols = xlRange.Columns;
-
-            //xLrange = (Excel.Range)(xlWorkSheet.UsedRange.Columns
-
-            int numRows = xlWorksheet.UsedRange.Rows.Count;
-            int numCols = xlWorksheet.UsedRange.Columns.Count;
-
-            double[,] xLarray = new double[numRows - 1, numCols];
-
-            for (int i = 2; i <= numRows; i++)
-            {
-                for (int j = 1; j <= numCols; j++)
-                {
-                    xLarray[i - 2, j - 1] = Convert.ToDouble(xlWorksheet.Cells[i, j].Value);
-                }
-            }
-
-            GC.Collect();
-            GC.WaitForPendingFinalizers();
-
-            Marshal.ReleaseComObject(xlRange);
-            Marshal.ReleaseComObject(xlWorksheet);
-
-            //close and release
-            xlWorkbook.Close();
-            Marshal.ReleaseComObject(xlWorkbook);
-
-            //quit and release
-            xlApp.Quit();
-            Marshal.ReleaseComObject(xlApp);
-
-
-
-            return xLarray;
-        }
-
-        public static int interpolateBinary(double[] inp,double x,int column)
-        {
-            int searchIndex; double upperVal; double lowerVal; 
-            int index = Array.BinarySearch(inp, x);
-            double interpolatedValue;
-            
-
-                if(index>0)
-                {
-                    searchIndex=index;
-                }
-                else
-                {
-                    searchIndex=~index;
-                }
-            
-            
-            return searchIndex;
-        }
-
-        public List<double> arrayToList(double[,] inp, int column)
-        {
-            var retList = new List<double>();
-
-            int numRow = inp.GetLength(0);
-            //int numCol = inp.GetLength(1);
-            for (int i = 0; i <= numRow; i++)
-            {
-                retList.Add(inp[i, column-1]);
-            }
-
-            return retList;
-        }
-    }
+   
     class Program
     {
         static void Main(string[] args)
@@ -207,6 +18,55 @@ namespace ConsoleApplication1
             double[,] designLinesArr = AAA.pullDesignLinesExcel();
 
             List<double> colDepths = AAA.arrayToList(designLinesArr, 1);
+            List<double> colDesign = AAA.arrayToList(designLinesArr, 2);
+            List<double> burDepths = AAA.arrayToList(designLinesArr, 3);
+            List<double> burDesign = AAA.arrayToList(designLinesArr, 4);
+            
+
+            // Inputs:
+                            double KOP = 4500;
+                            double DLS = 3.5;
+                            double buildsection = 2571;
+                            double finalinclination = 90;
+                            double MD = 9071;
+                            double intervalstep = 1;
+                            double MW = 10.6;
+                            double SFcollapse = 1.125;
+                            double SFjoint = 1.5;
+                            double SFyield = 1.5;
+                            double SFinternalyield = 1; //burst
+                            double Mu = 0.3;    //friction factor
+            
+            //  Build Trajectory:
+                            trajectory BBB = new trajectory();
+                            double[,] trajectoryarray = BBB.buildTrajectory(KOP,DLS,buildsection,finalinclination,MD,intervalstep);
+
+                        //    'Set current row to bottom of sheet
+                        //    currRow = Worksheets("Trajectory").UsedRange.Rows.Count
+    
+                        //    Open "D:\Completions\HW5\HW5Log.txt" For Output As #1
+                        //    Print #1, "StartLog"
+    
+    
+                        //    'for Last Row, set weight and max casing
+                        //   For currRow = Worksheets("Trajectory").UsedRange.Rows.Count To 3 Step -1
+                        //        Print #1, "MD= " & Worksheets("Trajectory").Cells(currRow, 1) & " FT"
+                        //        Call maxCasing(currRow)
+                        //        Call wBelow(currRow)
+                        //        Call collapseCheck(currRow)
+                        //        Call correctedcollapseCheck(currRow)
+                        //        Call burstCheck(currRow)
+                        //        Call bodyCheck(currRow)
+                        //        Call jointCheck(currRow)
+                        //        Call finalcollcheck(currRow)
+                        //        Print #1, "---------------------------------------------------------"
+                        //    Next currRow
+    
+                        //    Close #1
+
+                        //End Sub
+
+
 
             ////2d to 1d list (
             //int numRow = designLinesArr.GetLength(0);
@@ -217,15 +77,15 @@ namespace ConsoleApplication1
             //        list.Add(arr1[i, j]);
             //}
 
-            ////Test casing read from excel
-            //for (int i = 0; i < casingArr.GetLength(0); i++)
-            //{
-            //    for (int j = 0; j < casingArr.GetLength(1); j++)
-            //    {
-            //        Console.WriteLine(casingArr[i, j]);
-            //    }
-            //}
-            //Console.ReadLine();
+                            ////Test casing read from excel
+                            //for (int i = 0; i < trajectoryarray.GetLength(0); i++)
+                            //{
+                            //    for (int j = 0; j < trajectoryarray.GetLength(1); j++)
+                            //    {
+                            //        Console.WriteLine(trajectoryarray[i, j]);
+                            //    }
+                            //}
+                            //Console.ReadLine();
 
             ////test design lines read from excel
             //for (int i = 0; i < designLinesArr.GetLength(0); i++)
@@ -239,6 +99,16 @@ namespace ConsoleApplication1
             
             //Console.WriteLine("Hello, world!");
             //Console.ReadLine();
+
+            //////test lists
+            //for (int i = 0; i < colDepths.Count; i++)
+            //{
+                
+            //        Console.WriteLine(colDepths[i]);
+                
+            //}
+            //Console.ReadLine();
+
 
 
 
